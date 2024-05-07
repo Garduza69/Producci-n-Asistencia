29b87bf5d971acc1a2a62316bb9bf04d094795cd
Garduza69 <garduza9852@gmail.com>
2024-05-07 13:47:43 -0600

    Conexion

--- reportes/conexion2.php
+++ reportes/conexion2.php
@@ -1,8 +1,8 @@
 <?php
 $servidor= "localhost";
-$usuario= "u712195824_sistema";
-$password = "Cruzazul1443";
-$nombreBD= "u712195824_sistema";
+$usuario= "u712195824_sistema2";
+$password = "Cruzazul443";
+$nombreBD= "u712195824_sistema2";
 $db = new mysqli($servidor, $usuario, $password, $nombreBD);
 if ($db->connect_error) {
     die("la conexión ha fallado: " . $db->connect_error);
