83c6637609a6c7f825d4dcf047209e98cbadf2cb
Garduza69 <garduza9852@gmail.com>
2024-12-05 14:08:00 -0600

    Actualizacion a produccion

--- configuracion.php
+++ configuracion.php
@@ -3,7 +3,7 @@ require_once 'vendor/autoload.php';
 
 $clientID = '836660401451-fjm2ab434pvjbbusp700udsd0srne8q1.apps.googleusercontent.com';
 $clientSecret = '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';
-$redirectUri = 'https://universidadsotavento.com/autentificacion.php';
+$redirectUri = 'https://asistenciasus.site/autentificacion.php';
 
 // Crear el cliente de Google
 $client = new Google_Client();
